54a64b67586f05e69df132e6be87b0ad396f4ac3
mariabiancadossantosdasilva <165193978+mariabiancadossantosdasilva@users.noreply.github.com>
2024-04-05 14:01:04 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -2,10 +2,13 @@ let x = 0
 input.onGesture(Gesture.Shake, function () {
     x = randint(0, 2)
     if (x == 0) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
         basic.showIcon(IconNames.SmallSquare)
     } else if (x == 1) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
         basic.showIcon(IconNames.Square)
     } else {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
         basic.showIcon(IconNames.Scissors)
     }
 })
